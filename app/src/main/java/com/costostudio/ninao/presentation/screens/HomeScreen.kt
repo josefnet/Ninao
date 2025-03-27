245d551a1aa1d765950533724cc2eb80c00796c8
Youssef Ben Abdeljelil <benabdeljelil.youssef@gmail.com>
2025-03-27 02:43:58 +0100

    update register/login code, add logo & bg

--- app/src/main/java/com/costostudio/ninao/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/costostudio/ninao/presentation/screens/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.costostudio.ninao.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +14,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.costostudio.ninao.R
 import com.costostudio.ninao.presentation.viewmodel.HomeViewModel
 import com.google.firebase.auth.FirebaseAuth
 
@@ -25,33 +30,48 @@ import com.google.firebase.auth.FirebaseAuth
 fun HomeScreen(navController: NavController, viewModel: HomeViewModel) {
     val user = FirebaseAuth.getInstance().currentUser
 
+
     // Vérifie si l'utilisateur est connecté
     if (user != null) {
-        Column(
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(32.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Bienvenue ${user.email}",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+
+            Image(
+                painter = painterResource(id = R.drawable.bg),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.matchParentSize()
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Bouton de déconnexion
-            Button(
-                onClick = {
-                    viewModel.logout()
-                    navController.navigate("login_screen") {
-                        popUpTo("home_screen") { inclusive = true } // Ferme l'écran actuel
-                    }
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Se déconnecter")
+                Text(
+                    text = "Bienvenue ${user.email}",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Bouton de déconnexion
+                Button(
+                    onClick = {
+                        viewModel.logout()
+                        navController.navigate("login_screen") {
+                            popUpTo("home_screen") { inclusive = true } // Ferme l'écran actuel
+                        }
+                    }
+                ) {
+                    Text("Se déconnecter")
+                }
             }
         }
     } else {
