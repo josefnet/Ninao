d3f6462daa26349754a627daf4eb0586913c05bf
Youssef Ben Abdeljelil <benabdeljelil.youssef@gmail.com>
2025-06-03 17:40:48 +0200

    add SplashScreen using SplashScreen API (just for test)

---
+++ app/src/main/java/com/costostudio/ninao/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,21 @@
+package com.costostudio.ninao.presentation.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel : ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000) // Simule un chargement (auth, config, etc.)
+            _isLoading.value = false
+        }
+    }
+}
